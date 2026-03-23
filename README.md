2688fa063db926583f7d26446620b38860853f86
Gee <alickoamarko@gmail.com>
2026-03-23 20:46:08 +0100

    MVP for Windows?

---
+++ README.md
@@ -0,0 +1,49 @@
+Tool which installs dependencies (ALCOM, .NET, VPM, Unity Hub, Unity, ...) and prepares a VRChat Avatar project for you with latest recommended packages:
+- VRChat SDK Base
+- VRChat SDK Avatars
+- [VRCFury](https://vrcfury.com/getting-started) - general-purpose tool (connect stuff, manage Gestures, add Toggles, SPS, ...)
+- lilToon shader - shader which is depended on by many assets
+- [Modular Avatar](https://modular-avatar.nadena.dev/docs/tutorials/clothing) - attach clothing to avatar
+- GoGo Loco - complex avatar positioning in-game without needing full body tracking
+- [AAO: Avatar Optimizer](https://vpm.anatawa12.com/avatar-optimizer/en/docs/tutorial/basic-usage/) - avatar optimizer
+- [d4rkAvatarOptimizer](https://github.com/d4rkc0d3r/d4rkAvatarOptimizer/blob/main/README.md#how-to-use) - avatar optimizer
+- Gesture Manager - check results of hand gestures in editor (Quality of Life)
+
+
+# Usage
+
+Download -> Run as admin/root -> Open newly-created project in ALCOM (project will be named as current date and time)
+
+
+# Building
+
+(at least Python 3.14 is required)
+
+`pyinstaller -F VAC_Simplified.py`
+
+
+# Basics
+
+1) Find an avatar base on https://boothplorer.com/avatars (more items = better)
+(NOTE: **each avatar/asset has their own usage instructions on their pages on booth.pm** (click on red “View on BOOTH” button on the right in avatar’s Boothplorer page))
+2) Drag and drop into project scene
+3) Add a 'VRC Avatar Descriptor' component to it
+4) ^ set 'View Position' to between avatar's eyes
+5) 'VRChat SDK > Show Control Panel' (top-middle of window) -> build and upload
+
+(^ https://creators.vrchat.com/avatars/creating-your-first-avatar)
+
+
+# Extra info
+
+- Avatar optimization: https://vrwiki.info/wiki/Avatar_Optimization_Tutorial
+
+- Finding lewd assets (like naked body texture and pp): Go to your avatar’s page on https://boothplorer.com/ -> click on “+” icon -> set “R18/Adult” to “Show Only R18/Adult”
+
+- Position avatar without having full body tracking: Drag & drop GoGo Loco prefab onto avatar
+
+- Preview results of hand gestures in editor: Add `Gesture Manager` to scene then use it
+
+- Eye/face tracking support: https://docs.vrcft.io/docs/intro/getting-started
+
+- Synchronize Lovense toys with VRChat: https://osc.toys/getting-started
---
+++ VAC_Simplified.py
@@ -0,0 +1,203 @@
+import os
+import subprocess
+from datetime import datetime
+
+
+
+VRCFURY_REPO_URL = "https://vcc.vrcfury.com/"
+LILTOONSHADER_REPO_URL = "https://lilxyzw.github.io/vpm-repos/vpm.json"
+MODULARAVATAR_REPO_URL = "https://vpm.nadena.dev/vpm.json"
+AAOAVATAROPTIMIZER_REPO_URL = "https://vpm.anatawa12.com/vpm.json"
+D4RKAVATAROPTIMIZER_REPO_URL = "https://d4rkc0d3r.github.io/vpm-repos/main.json"
+GOGOLOCO_REPO_URL = "https://Spokeek.github.io/goloco/index.json"
+
+
+
+print("Installing/Updating ALCOM...")
+
+try:
+	if os.name == "nt":
+		subprocess.run([
+			"winget",
+			"install",
+			"anatawa12.ALCOM",
+			"--accept-package-agreements",
+			"--accept-source-agreements"
+		], check=True)
+	else:
+		pass # TODO: Implement for Linux
+except Exception as e:
+	if subprocess.run("winget list anatawa12.ALCOM").returncode != 0:
+		input(f"ERROR: Failed to install ALCOM\n{e}")
+		exit(1)
+
+
+print("Installing/Updating dotnet...")
+
+try:
+	if os.name == "nt":
+		subprocess.run([
+			"winget",
+			"install",
+			"Microsoft.Dotnet.SDK.8",
+			"--accept-package-agreements",
+			"--accept-source-agreements"
+		], check=True)
+	else: # NOTE: untested on Linux
+		subprocess.run("curl https://dot.net/v1/dotnet-install.sh | bash", check=True, shell=True)
+except Exception as e:
+	if subprocess.run("winget list Microsoft.Dotnet.SDK.8").returncode != 0:
+		input(f"ERROR: Failed to install dotnet\n{e}")
+		exit(1)
+
+os.reload_environ()
+
+
+print("Installing/Updating VRChat Package Manager...")
+
+try:
+	subprocess.run([
+		"dotnet",
+		"tool",
+		"install",
+		"--global",
+		"vrchat.vpm.cli"
+	], check=True)
+except Exception as e:
+	input(f"ERROR: Failed to install VRChat Package Manager\n{e}")
+	exit(1)
+
+os.reload_environ()
+
+
+print("Installing/Updating VPM templates...")
+
+try:
+	subprocess.run([
+		"vpm",
+		"install",
+		"templates"
+	], check=True)
+except Exception as e:
+	input(f"ERROR: Failed to install VPM templates\n{e}")
+	exit(1)
+
+
+print("Installing/Updating Unity Hub...")
+
+try:
+	subprocess.run([
+		"vpm",
+		"install",
+		"hub"
+	], check=True)
+except Exception as e:
+	input(f"ERROR: Failed to install Unity Hub (try running as admin/root?)\n{e}")
+	exit(1)
+
+os.reload_environ()
+
+
+print("Installing/Updating Unity...")
+
+try:
+	subprocess.run([
+		"vpm",
+		"install",
+		"unity"
+	], check=True)
+except Exception as e:
+	input(f"ERROR: Failed to install Unity\n{e}")
+	exit(1)
+
+os.reload_environ()
+
+
+print("Adding VPM repositories...")
+
+print("Adding VRCFury repo...")
+subprocess.run(f"vpm add repo {VRCFURY_REPO_URL}", check=True)
+
+print("Adding lilToon shader repo...")
+subprocess.run(f"vpm add repo {LILTOONSHADER_REPO_URL}", check=True)
+
+print("Adding Modular Avatar repo...")
+subprocess.run(f"vpm add repo {MODULARAVATAR_REPO_URL}", check=True)
+
+print("Adding AAO: Avatar Optimizer repo...")
+subprocess.run(f"vpm add repo {AAOAVATAROPTIMIZER_REPO_URL}", check=True)
+
+print("Adding d4rkAvatarOptimizer repo...")
+subprocess.run(f"vpm add repo {D4RKAVATAROPTIMIZER_REPO_URL}", check=True)
+
+print("Adding GoGo Loco repo...")
+subprocess.run(f"vpm add repo {GOGOLOCO_REPO_URL}", check=True)
+
+
+print("Creating new avatar project...")
+
+project_dir_name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+try:
+	subprocess.run([
+		"vpm",
+		"new",
+		project_dir_name,
+		"Avatar",
+		"-p",
+		"."
+	], check=True)
+except Exception as e:
+	input(f"ERROR: Failed to create new Avatar project\n{e}")
+	exit(1)
+
+os.chdir(project_dir_name)
+
+try:
+	subprocess.run([
+		"vpm",
+		"add",
+		"project",
+		os.getcwd()
+	], check=True)
+except Exception as e:
+	input(f"ERROR: Failed to add newly-created project to list of VCC projects\n{e}")
+	exit(1)
+
+
+print("Adding packages to avatar project...")
+
+print("Adding package: VRCFury...")
+subprocess.run(f"vpm add package com.vrcfury.vrcfury", check=True)
+
+print("Adding package: lilToon...")
+subprocess.run(f"vpm add package jp.lilxyzw.liltoon", check=True)
+
+print("Adding package: Modular Avatar...")
+subprocess.run(f"vpm add package nadena.dev.modular-avatar", check=True)
+
+print("Adding package: AAO: Avatar Optimizer...")
+subprocess.run(f"vpm add package com.anatawa12.avatar-optimizer", check=True)
+
+print("Adding package: d4rkAvatarOptimizer...")
+subprocess.run(f"vpm add package d4rkpl4y3r.d4rkavataroptimizer", check=True)
+
+print("Adding package: GoGoLoco...")
+subprocess.run(f"vpm add package gogoloco", check=True)
+
+print("Adding package: Gesture Manager...")
+subprocess.run(f"vpm add package vrchat.blackstartx.gesture-manager", check=True)
+
+
+print("Done!")
+
+
+input("""
+BASICS:
+
+1) Find an avatar base on https://boothplorer.com/avatars (more items = better)
+2) Drag and drop into project scene
+3) Add a 'VRC Avatar Descriptor' component to it
+4) ^ set 'View Position' to between avatar's eyes
+5) 'VRChat SDK > Show Control Panel' (top-middle of window) -> build and upload
+""")
